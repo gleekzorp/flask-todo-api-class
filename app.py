3e65e55a4bcf82ec38e9ba3792577bb006fdecd8
Daniel Floyd <gleek.dan@gmail.com>
2020-06-09 11:58:53 -0600

    added delete request to delete a todo based on id

--- app.py
+++ app.py
@@ -38,12 +38,9 @@ def hello():
 def add_todo():
     title = request.json['title']
     done = request.json['done']
-
     new_todo = Todo(title, done)
-
     db.session.add(new_todo)
     db.session.commit()
-
     todo = Todo.query.get(new_todo.id)
     return todo_schema.jsonify(todo)
 
@@ -52,7 +49,6 @@ def add_todo():
 def get_todos():
     all_todos = Todo.query.all()
     result = todos_schema.dump(all_todos)
-
     return jsonify(result)
 
 
@@ -63,6 +59,14 @@ def update_todo(id):
     todo.done = new_done
     db.session.commit()
     return todo_schema.jsonify(todo)
+
+
+@app.route("/todo/<id>", methods=["DELETE"])
+def delete_todo(id):
+    todo = Todo.query.get(id)
+    db.session.delete(todo)
+    db.session.commit()
+    return jsonify("Todo Deleted!")
 
 
 if __name__ == "__main__":
